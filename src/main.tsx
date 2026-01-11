8784cbdd31ca81cd38398627b93bc78f170abd23
Somij <qcconnect56gmail.com>
2026-01-11 17:05:11 +0400

    Fix missing index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './styles/index.css'
+import './index.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
- <BrowserRouter><App/></BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/qc-connect">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
