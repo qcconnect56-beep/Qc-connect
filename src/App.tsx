3779dc4b8f4f9637198e0da026bac196b1bbad2e
QC CONNECT <qcconnect56@gmail.com>
2026-03-10 22:32:18 +0400

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-
-import { Routes,Route } from 'react-router-dom'
-import WriteBlog from './pages/WriteBlog'
-export default ()=> (
- <Routes>
-  <Route path="/" element={<div className="p-6">Home</div>} />
-  <Route path="/write" element={<WriteBlog/>} />
- </Routes>
-)
